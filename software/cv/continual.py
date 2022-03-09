3a2440010f01bbb0e9feb8c9a5f117e7fa64ec3f
iBoot32 <iboot32@gmail.com>
2022-03-09 14:45:36 -0500

    refactor some code and prep to export cubestring

--- software/cv/continual.py
+++ software/cv/continual.py
@@ -3,8 +3,6 @@
 # uses opencv to read the stickers on a rubiks cube
 # for use with a solving algorithm such as cubebot2.0
 # supports calibrating colors for increased accuracy
-#
-# implement confidence metric based on how far away
 
 from calendar import c
 import math
@@ -17,6 +15,10 @@ centers = [
     [150, 75], [300, 75], [450, 75],
     [150, 225], [300, 225], [450, 225],
     [150, 375], [300, 375], [450, 375]
+]
+
+scan_to_cornerstring = [
+    []
 ]
 
 # stores calibrated colors
@@ -43,13 +45,16 @@ b = []
 color = [0, 0, 0] # r, g, b
 yes = (0,0,0)
 
+faces = []
+
 windowsize = 5
 smoothing = 8
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(1)
 
 def main():
     cald = False
+    scanned_faces = 0
     while(True):
           
         # Capture the video frame
@@ -58,8 +63,12 @@ def main():
         #img = cv2.flip(img,0)
         yes = img.shape
 
+        # draw circles for each center
+        for center_num in range (0, len(centers)):
+            cv2.circle(img, centers[center_num], 25, (0,0,255), 2)
+
         if cald:
-            side = 1
+            # state that we're calibrated, and also watch out for pressing "e"
             cv2.putText(img,"fully calibrated",(5,475), cv2.FONT_HERSHEY_SIMPLEX, .85, (0, 255, 255), 2, cv2.LINE_AA)
             cv2.putText(img,"fully calibrated",(5,475), cv2.FONT_HERSHEY_SIMPLEX, .85, (255, 0, 255), 1, cv2.LINE_AA)
             for circle in range(0, len(centers)):
@@ -75,52 +84,59 @@ def main():
                 # max euclidean distance is 255*sqrt(3), so calculate accuracy using that
                 #print(f'{detected_color} - confidence=[{round(100-((min_dist)*100)/(255*math.sqrt(3)),2)}%]')
                 confidence_metric.append(round(100-((min_dist)*100)/(255*math.sqrt(3)),2))
+
             pressedKey = cv2.waitKey(1) & 0xFF
             if pressedKey == ord('q'):
                 break
             if pressedKey == ord('e'):
-                for x in range(0, len(det_colors)):
-                    print(det_colors[x])
+                faces.append([w[0] for w in det_colors])
+                scanned_faces = scanned_faces + 1
+                if scanned_faces == 6:
+                    print("full scanned cube: ")
+                    for x in range(0, len(faces)):
+                        print(faces[x])
+            
             renderCube(det_colors, confidence_metric)
 
                 
-        
-        # draw circles for each center
-        for center_num in range (0, len(centers)):
-            cv2.circle(img, centers[center_num], 25, (0,0,255), 2)
-        
-        # check keys pressed
-        pressedKey = cv2.waitKey(1) & 0xFF
-        
-        if pressedKey == ord('q'):
-            break
+        else:
+            # if not calibrated, don't need to scan the cube or check if we want to scan a side
+            # basically just watch for "c" meaning to calibrate
+            
+            # check keys pressed
+            pressedKey = cv2.waitKey(1) & 0xFF
+            if pressedKey == ord('q'):
+                break
+            if pressedKey == ord('e'):
+                    faces.append([w[0] for w in det_colors])
+                    print(faces)
 
-        # calibrate colors
-        elif pressedKey == ord('c'):
-            scan_num = 0
-            while(True):
-                # need new feed of video since we are overlaying text and waiting for input
-                ret, img = vid.read()
-                #img = cv2.flip(img,0)
-                yes = img.shape
+            # calibrate colors
+            if pressedKey == ord('c'):
+                scan_num = 0
+                while(True):
+                    # need new feed of video since we are overlaying text and waiting for input
+                    ret, img = vid.read()
+                    #img = cv2.flip(img,0)
+                    yes = img.shape
 
-                # draw center circle and color to scan
-                center = (int(yes[1]/2), int(yes[0]/2))
-                cv2.circle(img, center, 25, (0,0,255), 2)
-                img = cv2.putText(img,colors[scan_num],(10,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 5, cv2.LINE_AA)
-                img = cv2.putText(img,colors[scan_num],(10,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 2, cv2.LINE_AA)
+                    # draw center circle and color to scan
+                    center = (int(yes[1]/2), int(yes[0]/2))
+                    cv2.circle(img, center, 25, (0,0,255), 2)
+                    img = cv2.putText(img,colors[scan_num],(10,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 5, cv2.LINE_AA)
+                    img = cv2.putText(img,colors[scan_num],(10,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 2, cv2.LINE_AA)
 
-                pressedKey = cv2.waitKey(1) & 0xFF
-                if pressedKey == ord('c'):
-                # if press c again, read color and store
-                    cal_color[scan_num] = scan(windowsize, img, center)
-                    scan_num = scan_num + 1 # increment the color we scan
-        
-                if pressedKey == ord('q') or scan_num == 6:
-                    cald = True
-                    break
+                    pressedKey = cv2.waitKey(1) & 0xFF
+                    if pressedKey == ord('c'):
+                    # if press c again, read color and store
+                        cal_color[scan_num] = scan(windowsize, img, center)
+                        scan_num = scan_num + 1 # increment the color we scan
+            
+                    if pressedKey == ord('q') or scan_num == 6:
+                        cald = True
+                        break
 
-                cv2.imshow("Output", img)
+                    cv2.imshow("Output", img)
         cv2.imshow("Output", img)
             
     vid.release()
@@ -160,8 +176,6 @@ def renderCube(colors, confidence):
     cv2.imshow("detect", rend)
     det_colors.clear()
     confidence_metric.clear()
-
-
 
 if __name__ == "__main__":
     main()
